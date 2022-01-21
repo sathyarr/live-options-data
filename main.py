2b93fb35c836eb4544fdff71ce14be5b1766f913
Sathya R <sathyacse67@gmail.com>
2022-01-21 19:39:40 +0530

    enhancement: add support for multiple scripts and option types

--- main.py
+++ main.py
@@ -5,19 +5,31 @@ from bs4 import BeautifulSoup
 
 from request_data_extractor import get_rows_and_headers
 
-data_url = "http://derivativefind.icicidirect.com/find20/Derivatives/FNOFundFlow/CompanyOptionTrends?symbol=NIFTY&optionType=CE"
 
-response = requests.get(data_url)
+def get_data(script_name, option_type):
+    data_url = 'http://derivativefind.icicidirect.com/find20/Derivatives/FNOFundFlow/CompanyOptionTrends?symbol='+script_name+'&optionType='+option_type
 
-soup = BeautifulSoup(response.content, 'html5lib')
-target_script_tag = soup.find('script', text=re.compile(r'datastr'))
+    response = requests.get(data_url)
 
-data_dict, data_headers = get_rows_and_headers(target_script_tag.text)
+    soup = BeautifulSoup(response.content, 'html5lib')
+    target_script_tag = soup.find('script', text=re.compile(r'datastr'))
 
-data_rows = []
-for row in data_dict['rows']:
-    data_rows.append(row['cell'])
+    data_dict, data_headers = get_rows_and_headers(target_script_tag.text)
 
-data_df = pd.DataFrame(data_rows, columns=data_headers)
+    data_rows = []
+    for row in data_dict['rows']:
+        data_rows.append(row['cell'])
+
+    data_df = pd.DataFrame(data_rows, columns=data_headers)
+    return data_df
+
+
+scripts_list = ['NIFTY', 'BANKNIFTY']
+
+for script in scripts_list:
+    ce_data = get_data(script, 'CE')
+    pe_data = get_data(script, 'PE')
+    ce_pe_data = ce_data.append(pe_data)
+    print()
 
 print()
